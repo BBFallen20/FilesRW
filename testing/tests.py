4e79fc23141a0ec73c435242150a41cbcba12f4f
BBFallen20 <blackbloodfallen123@gmail.com>
2021-07-27 10:40:15 +0300

    Tests extended and refactored.

--- testing/tests.py
+++ testing/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from handlers import TXTHandler, CSVHandler, XLSXHandler
+from handlers import TXTHandler, CSVHandler, XLSXHandler, ExtensionHandler
 from main import choose_mode
 
 
@@ -9,42 +9,71 @@ class TestAppLogic(unittest.TestCase):
         self.assertFalse(choose_mode('log', '/home'))
         self.assertFalse(choose_mode('any_mode', '/home'))
 
-    def test_read_file(self):
+    def test_TXTHandler(self):
+        # Read method testing
+
         # No such file testing
         self.assertFalse(TXTHandler('/home/file123.txt').read())
-        self.assertFalse(CSVHandler('/home/file123.txt').read())
-        self.assertFalse(XLSXHandler('/home/file123.txt').read())
-
         # Directory as file path testing
         self.assertFalse(TXTHandler('/home').read())
-        self.assertFalse(CSVHandler('/home').read())
-        self.assertFalse(XLSXHandler('/home').read())
-
         # Empty TXT file opening test
-        self.assertFalse(TXTHandler('/home/user/PycharmProjects/FilesRW/testing/empty.txt').read())
+        self.assertFalse(TXTHandler('/home/user/PycharmProjects/FilesRW/testing/files/empty.txt').read())
         # Non empty TXT file opening test(only 3 lines)
-        self.assertTrue(TXTHandler('/home/user/PycharmProjects/FilesRW/testing/file1.txt').read())
+        self.assertTrue(TXTHandler('/home/user/PycharmProjects/FilesRW/testing/files/file1.txt').read())
 
-        # Empty CSV file opening test
-        self.assertFalse(CSVHandler('/home/user/PycharmProjects/FilesRW/testing/empty.csv').read())
-        # Non empty CSV file opening test
-        self.assertTrue(CSVHandler('/home/user/PycharmProjects/FilesRW/testing/100 Sales Records.csv').read())
+        # Write method test
 
-        # Empty XLSX file opening test
-        self.assertFalse(XLSXHandler('/home/user/PycharmProjects/FilesRW/testing/empty.xlsx').read())
-        # Non empty XLSX file opening test
-        self.assertTrue(XLSXHandler('/home/user/PycharmProjects/FilesRW/testing/test.xlsx').read())
-
-    def test_write_to_file(self):
         # No permissions test
         self.assertFalse(TXTHandler('home/file.txt').write())
-        self.assertFalse(CSVHandler('home/file.txt').write())
-        self.assertFalse(XLSXHandler('home/file.txt').write())
-
         # No such file test
         self.assertFalse(TXTHandler('/home/user/folder/empty.txt').write())
+
+    def test_CSVHandler(self):
+        # Read method testing
+
+        # No such file testing
+        self.assertFalse(CSVHandler('/home/file123.txt').read())
+        # Directory as file path testing
+        self.assertFalse(CSVHandler('/home').read())
+        # Empty CSV file opening test
+        self.assertFalse(CSVHandler('/home/user/PycharmProjects/FilesRW/testing/files/empty.csv').read())
+        # Non empty CSV file opening test
+        self.assertTrue(CSVHandler('/home/user/PycharmProjects/FilesRW/testing/files/100 Sales Records.csv').read())
+
+        # Write method testing
+
+        # No permissions test
+        self.assertFalse(CSVHandler('home/file.txt').write())
+        # No such file test
         self.assertFalse(CSVHandler('/home/user/folder/empty.txt').write())
+
+    def test_XLSXHandler(self):
+        # Read method testing
+
+        # No such file testing
+        self.assertFalse(XLSXHandler('/home/file123.txt').read())
+        # Directory as file path testing
+        self.assertFalse(XLSXHandler('/home').read())
+        # Empty XLSX file opening test
+        self.assertFalse(XLSXHandler('/home/user/PycharmProjects/FilesRW/testing/files/empty.xlsx').read())
+        # Non empty XLSX file opening test
+        self.assertTrue(XLSXHandler('/home/user/PycharmProjects/FilesRW/testing/files/test.xlsx').read())
+
+        # Write method testing
+
+        # No permissions test
+        self.assertFalse(XLSXHandler('home/file.txt').write())
+        # No such file test
         self.assertFalse(XLSXHandler('/home/user/folder/empty.txt').write())
+
+    def test_ExtensionHandler_error_wrapper(self):
+        wrapper = ExtensionHandler.error_wrapper
+        # No such file
+        self.assert_(wrapper(TXTHandler('/home/user/folder/empty.txt').read()))
+        # Empty file reading
+        self.assert_(wrapper(TXTHandler('/home/user/PycharmProjects/FilesRW/testing/files/empty.txt').read()))
+        # Directory as a path
+        self.assert_(wrapper(TXTHandler('/home/user/folder').read()))
 
 
 if __name__ == '__main__':
